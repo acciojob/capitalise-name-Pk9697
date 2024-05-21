6d4bf92a5b9518716460e6c43549d33d7038e253
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-05-21 12:37:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const inputEle=document.getElementById('fname')
+
+inputEle.addEventListener('focusout',function (e) {
+	inputEle.value=inputEle.value.toUpperCase()
+})
